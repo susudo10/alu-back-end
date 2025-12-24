85b2b68ccaf3768f6043a0fd3bdb6879cfa8b44e
susudo10 <k.leparwa@alustudent.com>
2025-12-24 11:14:05 +0300

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""Script to use a REST API for a given employee ID, returns
-information about his/her TODO list progress and export in CSV"""
-import csv
+"""Displays employee TODO list progress"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print(f"UsageError: python3 {__file__} employee_id(int)")
         sys.exit(1)
 
-    API_URL = "https://jsonplaceholder.typicode.com"
     EMPLOYEE_ID = sys.argv[1]
+    API_URL = "https://jsonplaceholder.typicode.com"
 
-    response = requests.get(
-        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
-        params={"_expand": "user"}
+    user = requests.get(f"{API_URL}/users/{EMPLOYEE_ID}").json()
+    todos = requests.get(
+        f"{API_URL}/todos",
+        params={"userId": EMPLOYEE_ID}
+    ).json()
+
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task["completed"]]
+
+    print(
+        f"Employee {employee_name} is done with tasks"
+        f"({len(done_tasks)}/{total_tasks}):"
     )
-    data = response.json()
 
-    if not len(data):
-        print("RequestError:", 404)
-        sys.exit(1)
-
-    username = data[0]["user"]["username"]
-
-    with open(f"{EMPLOYEE_ID}.csv", "w", newline="") as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
-        for task in data:
-            writer.writerow(
-                [EMPLOYEE_ID, username, str(task["completed"]), task["title"]]
-            )
+    for task in done_tasks:
+        print(f"\t {task['title']}")
